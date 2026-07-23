532a5abb78fb9938cea02dc7c6b1f1f7490db8eb
zagaaam-ops <zagaaam@gmail.com>
2026-07-23 14:05:21 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    const ProviderScope(
+      child: FastFileOrganizerApp(),
+    ),
+  );
+}
