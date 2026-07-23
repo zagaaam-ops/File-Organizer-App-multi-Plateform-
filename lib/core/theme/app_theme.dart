a7ffa8b81f8ed2b7e9c14d85b2e3c1db511d6d51
zagaaam-ops <zagaaam@gmail.com>
2026-07-23 14:44:44 +0300

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,27 @@
 
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData light() {
+    return ThemeData(
+      useMaterial3: true,
+
+      colorSchemeSeed: AppColors.primary,
+
+      scaffoldBackgroundColor: AppColors.background,
+
+      appBarTheme: const AppBarTheme(
+        centerTitle: false,
+        elevation: 0,
+      ),
+    );
+  }
+
+  static ThemeData dark() {
+    return ThemeData.dark(useMaterial3: true);
+  }
+}
