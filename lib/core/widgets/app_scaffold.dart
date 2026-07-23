3bd366d301dc733f278bcf461f5a817a47e8359b
zagaaam-ops <zagaaam@gmail.com>
2026-07-23 15:08:13 +0300

    Update app_scaffold.dart

--- lib/core/widgets/app_scaffold.dart
+++ lib/core/widgets/app_scaffold.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({
+    super.key,
+    required this.body,
+    this.toolbar,
+    this.statusBar,
+  });
+
+  final Widget body;
+  final PreferredSizeWidget? toolbar;
+  final Widget? statusBar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: toolbar,
+      body: body,
+      bottomNavigationBar: statusBar,
+    );
+  }
+}
