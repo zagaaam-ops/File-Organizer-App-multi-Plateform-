032729c80f5e86e6579447fe852292370b802f05
zagaaam-ops <zagaaam@gmail.com>
2026-07-23 15:17:34 +0300

    Update home_page.dart

--- lib/features/organizer/presentation/pages/home_page.dart
+++ lib/features/organizer/presentation/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import '../../../../core/widgets/app_scaffold.dart';
 import '../../../../core/widgets/app_toolbar.dart';
 import '../../../../core/widgets/status_bar.dart';
+import '../widgets/destination_panel.dart';
+import '../widgets/file_panel.dart';
 import '../widgets/split_view.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,42 +16,8 @@ class HomePage extends StatelessWidget {
       toolbar: AppToolbar(),
       statusBar: StatusBar(),
       body: SplitView(
-        left: _FilesPanel(),
-        right: _DestinationPanel(),
-      ),
-    );
-  }
-}
-
-class _FilesPanel extends StatelessWidget {
-  const _FilesPanel();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text(
-          "Files Panel\n\n(Coming in Commit 004)",
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-  }
-}
-
-class _DestinationPanel extends StatelessWidget {
-  const _DestinationPanel();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xfff5f5f5),
-      child: const Center(
-        child: Text(
-          "Destination Panel\n\n(Coming in Commit 004)",
-          textAlign: TextAlign.center,
-        ),
+        left: FilePanel(),
+        right: DestinationPanel(),
       ),
     );
   }
