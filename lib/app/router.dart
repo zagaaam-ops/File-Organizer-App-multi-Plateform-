6810eb2c8f59d7ca5029438ed6b0564b7fcc04e8
zagaaam-ops <zagaaam@gmail.com>
2026-07-23 15:11:33 +0300

    Update router.dart

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,21 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+import '../features/organizer/presentation/pages/home_page.dart';
 
 class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) {
-          return const Scaffold(
-            body: Center(
-              child: Text(
-                'Fast File Organizer\n\nVersion 0.1.0',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          );
-        },
+        builder: (context, state) => const HomePage(),
       ),
     ],
   );
